c110ed93a7961f6f3eb91312dfc22907aa81fea2
unknown <mhammad1991h@gmail.com>
2020-03-17 01:43:42 +0100

    opdracht 1

---
+++ eerste.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Radio</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+  </head>
+  <body>
+    <div class="wrapper">
+      <div class="header">
+          <nav>
+            <input type="checkbox" id="check">
+            <label for="check" class="checkbtn">
+              <i class="fas fa-bars"></i>
+            </label>
+            <label class="logo">Kraeken en Krønen</label>
+            <ul>
+              <li><a class="active" href="#">Home</a></li>
+              <li><a href="#">Contact</a></li>
+              <li><a href="#">Zenders</a></li>
+            </ul>
+          </nav>
+       </div>   
+       <div class="content">
+        <img src="images/dj.png">
+        <img src="images/dj1.png">
+        <img src="images/dj2.png">
+       </div>
+       
+       <div class="footer">
+        <p>All right by kreake&kronen</p>
+       </div>
+    </div>
+  </body>
+</html>
